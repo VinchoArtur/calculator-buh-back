72f3f03e8ca0972f93699b5cad70af60fdb8ae50
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-10 01:08:12 +0300

    refactor: persists cost data to a JSON file

--- src/services/add-costs/add-costs.service.ts
+++ src/services/add-costs/add-costs.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { writeFile } from 'fs/promises';
+import { existsSync } from 'fs';
+import { mkdir, readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 import { CostRequest } from 'src/dtos/costs/i-costs';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class AddCostsService {
+  private readonly assetsFolder = join(__dirname, '..', '..', '..', 'assets');
+  private readonly fileName = `data-${Date.now()}.json`;
+  private readonly filePath = join(this.assetsFolder, this.fileName);
   public async addCosts(cost: CostRequest) {
-    const fileName = `data-${Date.now()}.json`;
-    const filePath = join(__dirname, '..', '..', '..', 'assets', fileName);
-    await writeFile(filePath, JSON.stringify(cost, null, 2), 'utf8');
+    const newCost: CostRequest = { ...cost, id: v4() };
 
-    return filePath;
+    if (!existsSync(this.assetsFolder)) {
+      await mkdir(this.assetsFolder, { recursive: true });
+    }
+
+    let costs: CostRequest[] = [];
+
+    if (existsSync(this.filePath)) {
+      const existingData = await readFile(this.filePath, 'utf8');
+      costs = JSON.parse(existingData);
+    }
+
+    costs.push(newCost);
+
+    await writeFile(this.filePath, JSON.stringify(costs, null, 2), 'utf8');
+
+    return `Cost added successfully! Saved at: ${this.filePath}`;
   }
 }
