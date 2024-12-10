328104c3a81eabd8e0a4a399ac7780e06811b478
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-10 15:04:26 +0300

    feat: adds endpoint to retrieve costs

--- src/controllers/add-costs/add-costs.controller.ts
+++ src/controllers/add-costs/add-costs.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CostRequest } from 'src/dtos/costs/i-costs';
 import { AddCostsService } from 'src/services/add-costs/add-costs.service';
 
 @Controller('add-costs')
 export class AddCostsController {
   constructor(private readonly addCostService: AddCostsService) {}
+
   @Post()
   async addCost(@Body() data: CostRequest) {
     try {
       return await this.addCostService.addCosts(data);
+    } catch (error) {
+      return `error: ${error.message}`;
+    }
+  }
+
+  @Get()
+  async getCosts() {
+    try {
+      return await this.addCostService.getCosts();
     } catch (error) {
       return `error: ${error.message}`;
     }
