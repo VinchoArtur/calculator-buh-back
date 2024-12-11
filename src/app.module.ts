e1faf51e24c61c72d85eec5bd8f87c198bc14ca4
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-12 00:23:51 +0300

    feat: adds FileService to AppModule providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AddCostsController } from './controllers/add-costs/add-costs.controller';
 import { AddCostsService } from './services/add-costs/add-costs.service';
+import { FileService } from './controllers/file/file.service';
 
 @Module({
   imports: [],
   controllers: [AppController, AddCostsController],
-  providers: [AppService, AddCostsService],
+  providers: [AppService, AddCostsService, FileService],
 })
 export class AppModule {}
