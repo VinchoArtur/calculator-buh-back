674a904f27588fa6dc0d4c412f7d67a0a5b031ce
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-12 00:22:43 +0300

    feat: adds endpoint to update costs

--- src/controllers/add-costs/add-costs.controller.ts
+++ src/controllers/add-costs/add-costs.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Logger,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { CostRequest } from 'src/dtos/costs/i-costs';
@@ -44,5 +45,13 @@ export class AddCostsController {
       this.logger.error('Error while deleting cost:', error.message);
       throw error;
     }
+  }
+
+  @Patch(':id')
+  async updateCost(
+    @Param('id') id: string,
+    @Body() updatedCost: Partial<CostRequest>,
+  ): Promise<CostRequest> {
+    return await this.addCostService.updateCost(id, updatedCost);
   }
 }
