a9340eefb4290875e4fb7aeb42ad1b0a997e4b6b
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-12 22:55:20 +0300

    feat: adds authentication controller and service to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AddCostsController } from './controllers/add-costs/add-costs.controller';
 import { AddCostsService } from './services/add-costs/add-costs.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, AddCostsController],
-  providers: [AppService, AddCostsService],
+  controllers: [AppController, AddCostsController, AuthController],
+  providers: [AppService, AddCostsService, AuthService],
 })
 export class AppModule {}
