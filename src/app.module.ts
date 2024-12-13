40da20bcf5176c5cca70f472a3edfd716be35f6c
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-14 01:00:17 +0300

    refactor: refactors cost module and adds presents module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AddCostsController } from './controllers/add-costs/add-costs.controller';
-import { AddCostsService } from './services/add-costs/add-costs.service';
+import { CostsController } from './controllers/costs/costs.controller';
+import { CostsService } from './services/costs/costs.service';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { FileService } from './services/file/file.service';
+import { PresentsController } from './controllers/presents/presents.controller';
+import { PresentsService } from './services/presents/presents.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, AddCostsController, AuthController],
-  providers: [AppService, AddCostsService, AuthService, FileService],
+  controllers: [
+    AppController,
+    CostsController,
+    AuthController,
+    PresentsController,
+  ],
+  providers: [
+    AppService,
+    CostsService,
+    AuthService,
+    FileService,
+    PresentsService,
+  ],
 })
 export class AppModule {}
